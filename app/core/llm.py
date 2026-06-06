c71e8dceda2910dd2ec8c1c5d7f9bd4ce2a3faa5
PRAJWAL KIRAN SHARMA <prajwalkiransharma@PRAJWALs-Mac-mini.local>
2026-06-06 22:20:41 +0530

    Structured Output

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,38 +1,16 @@
-# from app.core.openai_client import client
-# from app.models.agent_response import AgentResponse
-
-
-# def generate(
-#     messages: list[dict],
-# ) -> AgentResponse:
-
-#     response = client.responses.parse(
-#         model="gpt-5-mini",
-#         input=messages,
-#         text_format=AgentResponse,
-#     )
-
-#     return response.output_parsed
-
-from app.models.agent_response import AgentResponse
 from app.core.openai_client import client
+from app.models.agent_response import AgentResponse
 
 
 def generate(
     messages: list[dict],
 ) -> AgentResponse:
 
-    response = client.responses.create(
+    response = client.responses.parse(
         model="gpt-5-mini",
         input=messages,
+        text_format=AgentResponse,
     )
 
-    return AgentResponse(
-        response=response.output_text,
-        handoff=False,
-        media=[],
-        is_confirmation=False,
-        suggested_question=None,
-        service_catalog=None,
-        data_used=[],
-    )
+    return response.output_parsed
+
